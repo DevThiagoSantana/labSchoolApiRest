bec2fbbc9df10ab3df4de157b4e8a7e1e1bb4882
Thiago de Santana <113400543+DevThiagoSantana@users.noreply.github.com>
2022-11-27 15:26:12 -0300

    Insersão inicial

---
+++ LabSchoolAPiRest/src/main/java/tech/devinhouse/labschoolapirest/LabSchoolAPiRestApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.labschoolapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSchoolAPiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSchoolAPiRestApplication.class, args);
+    }
+
+}
---
+++ LabSchoolAPiRest/src/test/java/tech/devinhouse/labschoolapirest/LabSchoolAPiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.labschoolapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSchoolAPiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
